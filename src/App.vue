7fc60b27d50df9555de95e3ffc634c100c507205
twnweijia <1115892484@qq.com>
2019-01-24 20:03:22 +0800

    更改首页冲突

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,27 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Xheader/>
-=======
-   <Tsettop/>
->>>>>>> a0c58f9614b25372a5e149a4425424932da17614
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 // import HelloWorld from './components/HelloWorld.vue'
 import Xheader from './components/Xheader.vue'
 
-=======
-import Tsettop from "./components/Tsettop.vue";
->>>>>>> a0c58f9614b25372a5e149a4425424932da17614
 export default {
   name: "app",
   components: {
-<<<<<<< HEAD
     // HelloWorld
     Xheader
-=======
-    Tsettop
->>>>>>> a0c58f9614b25372a5e149a4425424932da17614
   }
 };
 </script>
 
 <style>
-<<<<<<< HEAD
 /*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-=======
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
->>>>>>> a0c58f9614b25372a5e149a4425424932da17614
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
