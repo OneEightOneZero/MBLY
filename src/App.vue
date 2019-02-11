430303d55526e686c0016cc67a32b220c5b5d687
huangrongwei <1300369969@qq.com>
2019-02-11 21:30:26 +0800

    添加静态登陆页

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <Xidxheader/>
+    <Xidxbanner/>
+    <Xidxhottrip/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import Xheader from './components/Xheader.vue'
-import Xidxheader from './components/Xidxheader.vue'
+import Xidxheader from "./components/Xidxheader.vue";
+import Xidxbanner from "./components/Xidxbanner.vue";
+import Xidxhottrip from "./components/Xidxhottrip.vue";
 
 export default {
   name: "app",
   components: {
-      Xidxheader
+    Xidxheader,
+    Xidxbanner,
+    Xidxhottrip
   }
 };
 </script>
 
 <style>
-
 /*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
