eae98fcf8a1357a42ff755ae37aba85895833cd8
twnweijia <1115892484@qq.com>
2019-02-13 17:40:28 +0800

    小改动

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Xidxheader/>
     <Xidxbanner/>
     <Xidxhottrip/>
+    <!-- <Xidxhottrip1/> -->
   </div>
 </template>
 
@@ -10,13 +11,17 @@
 import Xidxheader from './components/Xidxheader.vue'
 import Xidxbanner from './components/Xidxbanner.vue'
 import Xidxhottrip from './components/Xidxhottrip.vue'
+//请求数据的草稿，先别删
+// import Xidxhottrip1 from './components/Xidxhottrip.1.vue'
 
 export default {
   name: "app",
   components: {
     Xidxheader,
     Xidxbanner,
-    Xidxhottrip
+    Xidxhottrip,
+    // Xidxhottrip1
+
   }
 };
 </script>
